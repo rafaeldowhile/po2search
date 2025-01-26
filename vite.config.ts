ee07c7ca146b29f30ebc478ec0f8415de11157a5
Rafael Fonseca <rafael@superstorm.dev>
2025-01-26 17:02:30 -0300

    remove route config

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_lazyRouteDiscovery: true,
-        v3_routeConfig: true,
         v3_singleFetch: true,
         unstable_optimizeDeps: true,
       },
